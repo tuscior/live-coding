ec9e2f7837a7f398ddda3a25b4cfca1a51b6a616
tuscior <tluscik.k@gmail.com>
2024-11-25 10:41:10 +0100

    some validation

--- controllers/TradeController.ts
+++ controllers/TradeController.ts
@@ -5,16 +5,19 @@ import { BinanceValidationError } from '../utils/error';
 const getTrades = (tradeService: TradeService) => async (req: Request, res: Response, next: NextFunction) => {
     const symbol = req.params.symbol;
     if(!symbol?.trim){
-        res.status(404).json({ message: "You provided wrong symbol for this endpoint" })
+        res.status(400).json({ message: "You provided wrong symbol for this endpoint" })
     }
     try {
         const trades = await tradeService.getTrades(symbol);
-        res.status(200).json(trades)
+        res.status(200).json(trades);
+        return;
     } catch(err) {
         if(err instanceof BinanceValidationError) {
             res.status(404).json({ message: "You provided wrong symbol for this endpoint" });
+            return;
         }
-        res.status(500).json({ message: "Something went wrong. Please try again" })
+        res.status(500).json({ message: "Something went wrong. Please try again" });
+        return;
     }
     
 }
--- services/TradeService.ts
+++ services/TradeService.ts
@@ -1,5 +1,6 @@
 import { AppConfig } from "../utils/config";
 import fetch from 'node-fetch';
+import { BinanceValidationError } from "../utils/error";
 
 export class TradeService {
     constructor(private config: AppConfig){
@@ -8,6 +9,9 @@ export class TradeService {
     // Fetch historical market data for a specific cryptocurrency symbol and time range using the API. 
     async getTrades(symbol: string){
         const data = await fetch(`${this.config.BINANCE_URL}?symbol=${symbol}`);
+        if(data.status === 400){
+            throw new BinanceValidationError()
+        }
         const trades = await data.json();
         return trades;
     }
