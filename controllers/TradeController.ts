cedbbeaf9c27208e37dca8316a9a024c20c420b5
tuscior <tluscik.k@gmail.com>
2024-11-25 10:37:57 +0100

    fetching tradfes works

--- controllers/TradeController.ts
+++ controllers/TradeController.ts
@@ -1,5 +1,6 @@
 import { Router, Request, Response, NextFunction } from 'express'
 import { TradeService } from '../services/TradeService';
+import { BinanceValidationError } from '../utils/error';
 
 const getTrades = (tradeService: TradeService) => async (req: Request, res: Response, next: NextFunction) => {
     const symbol = req.params.symbol;
@@ -8,9 +9,12 @@ const getTrades = (tradeService: TradeService) => async (req: Request, res: Resp
     }
     try {
         const trades = await tradeService.getTrades(symbol);
-
+        res.status(200).json(trades)
     } catch(err) {
-        
+        if(err instanceof BinanceValidationError) {
+            res.status(404).json({ message: "You provided wrong symbol for this endpoint" });
+        }
+        res.status(500).json({ message: "Something went wrong. Please try again" })
     }
     
 }
---
+++ utils/error.ts
@@ -0,0 +1,5 @@
+export class BinanceValidationError extends Error {
+    constructor(){
+        super('Binance Validation Error')
+    }
+}
